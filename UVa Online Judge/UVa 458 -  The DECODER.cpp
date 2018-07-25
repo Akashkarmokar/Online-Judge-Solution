eb88385abe5e5ae9bedc339e7131ba3ddad4a899
Akash Karmokar <akashcsemu@gmail.com>
2018-07-25 14:37:52 +0600

    Add files via upload

---
+++ UVa Online Judge/UVa 458 -  The DECODER.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    char c;
+    while(scanf("%c",&c) != EOF){
+        if(c!='\n'){
+            printf("%c",c-7);
+        }
+        else{
+            printf("\n");
+        }
+    }
+}
