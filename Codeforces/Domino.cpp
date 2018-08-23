dba9d8949d84fdbb84fe2d166ebc6668a9506813
Akash Karmokar <akashcsemu@gmail.com>
2018-08-24 02:30:34 +0600

    Add files via upload

---
+++ Codeforces/Domino.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int m,n;
+    cin>>m>>n;
+    cout<<(m*n)/2<<endl;
+    return 0;
+}
+/*
+*   Chant Hare Krishna mohamontra and be happy .
+*
+*             Akash Karmokar
+*   Computer Science and engineering Department
+*             38th Batch
+* Metropolitan University,Sylhet,Bangladesh.
+* fb:https://www.facebook.com/akashcsemu
+*
+*/
