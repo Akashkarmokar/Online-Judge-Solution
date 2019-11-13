b2c0cc7e573dc0be8e2cfa453ad5bb3a47dc590f
Akash Karmokar <akashcsemu@gmail.com>
2019-11-13 08:00:17 +0600

    Add files via upload

---
+++ UVa Online Judge/1124.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include<cstdio>
+using namespace std;
+ 
+//________________________________________________________________________________________________________
+#define fastread ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define in freopen("input.txt","r",stdin)
+#define out freopen("output.txt","w",stdout);
+//________________________________________________________________________________________________________
+ 
+string s;
+//--------------------------------------------------------------------------------------------------------
+int main()
+{
+    // in;
+    //out;
+    fastread;
+    while(getline(cin,s))
+    {
+        cout<<s<<endl;
+    }
+    return 0;
+}
